807aac6faf77e7ccd85fa12ff73605573f12ba60
Philip Tzou <philip.npc@gmail.com>
2022-07-31 18:38:55 -0700

    Fix make-slim-version.sh too

--- scripts/make-slim-version.sh
+++ scripts/make-slim-version.sh
@@ -6,40 +6,10 @@ sqlite3 $DB_FILE <<EOF
 DELETE FROM isolates;
 DELETE FROM isolate_mutations;
 DELETE FROM isolate_excluded_surv_mutations;
-DELETE FROM articles AS ref WHERE EXISTS (
-  SELECT 1 FROM article_annotations refnote WHERE
-    ref.ref_name = refnote.ref_name AND
-    refnote.status IN ('EX', 'New')
-);
-DELETE FROM article_summaries AS ref WHERE EXISTS (
-  SELECT 1 FROM article_annotations refnote WHERE
-    ref.ref_name = refnote.ref_name AND
-    refnote.status IN ('EX', 'New')
-);
-DELETE FROM dataset_summaries AS ref WHERE EXISTS (
-  SELECT 1 FROM article_annotations refnote WHERE
-    ref.ref_name = refnote.ref_name AND
-    refnote.status IN ('EX', 'New')
-);
-DELETE FROM dataset_gene_summaries AS ref WHERE EXISTS (
-  SELECT 1 FROM article_annotations refnote WHERE
-    ref.ref_name = refnote.ref_name AND
-    refnote.status IN ('EX', 'New')
-);
-DELETE FROM dataset_drug_class_summaries AS ref WHERE EXISTS (
-  SELECT 1 FROM article_annotations refnote WHERE
-    ref.ref_name = refnote.ref_name AND
-    refnote.status IN ('EX', 'New')
-);
-DELETE FROM dataset_subtype_summaries AS ref WHERE EXISTS (
-  SELECT 1 FROM article_annotations refnote WHERE
-    ref.ref_name = refnote.ref_name AND
-    refnote.status IN ('EX', 'New')
-);
-DELETE FROM dataset_surv_mutation_summaries AS ref WHERE EXISTS (
-  SELECT 1 FROM article_annotations refnote WHERE
-    ref.ref_name = refnote.ref_name AND
-    refnote.status IN ('EX', 'New')
+DELETE FROM articles AS ref WHERE NOT EXISTS (
+  SELECT 1 FROM datasets ds WHERE
+    ref.ref_name = ds.ref_name AND
+    ds.status NOT IN ('EX', 'New')
 );
 VACUUM;
 EOF
